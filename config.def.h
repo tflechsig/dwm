779ba2c147d4120a09016d138cc997253f5e74df
flechsig <tflechsig27@gmail.com>
2023-01-05 22:19:44 -0500

    Add barpadding patch

--- config.def.h
+++ config.def.h
@@ -2,10 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 18;       /* gap pixel between windows */
+static const unsigned int gappx     = 10;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=12" };
 static const char col_gray1[]       = "#2E3440";
