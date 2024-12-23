c313a517fb79004ea2afb8371264ec6a78b71a61
flechsig <tflechsig27@gmail.com>
2024-12-22 22:57:29 -0500

    Update dwmblocks submodule

--- config.def.h
+++ config.def.h
@@ -26,7 +26,7 @@ static const char *colors[][3]        = {
 /* tagging */
 static const int num_tags = 4;
 /*                                  Active   Inactive */
-static const char *tag_symbols[] = {""    , ""    };
+static const char *tag_symbols[] = {"", ""};
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
